1fc9238dcee3306b0ad9688a89164413875ea16a
Nathan Dammerich <natedammerich@gmail.com>
2021-12-29 03:43:25 -0500

    Heroku commit

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ const PORT = process.env.PORT || 5000;
 mongoose
   .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() =>
-    app.listen(process.env.PORT, () =>
+    app.listen(PORT, () =>
       console.log(`Server Running on Port: http://localhost:${PORT}`)
     )
   )
