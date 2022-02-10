5045d2238409e85ffb5779d9c44b702afed65cd3
Nathan Dammerich <natedammerich@gmail.com>
2022-02-09 21:50:38 -0500

    f

--- redis_connect.js
+++ redis_connect.js
@@ -2,7 +2,7 @@ import Redis from "ioredis";
 import dotenv from "dotenv";
 dotenv.config();
 
-export const client = new Redis(process.env.REDIS_TLS_URL, {
+export const client = new Redis(process.env.REDIS_URL, {
   tls: {
     rejectUnauthorized: false,
   },
