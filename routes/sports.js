c68fbd4a604be0dc0305d64ff22a6f1da4caa7a1
matt <mattnestler96@gmail.com>
2022-02-11 15:57:53 -0800

    list sports

--- routes/sports.js
+++ routes/sports.js
@@ -1,6 +1,11 @@
 import express from "express";
 
-import { getSport, updateSport, createSport } from "../controllers/sports.js";
+import {
+  getSport,
+  updateSport,
+  createSport,
+  listSports,
+} from "../controllers/sports.js";
 
 import { verifyToken } from "../middleware/authenticateToken.js";
 
@@ -9,5 +14,6 @@ const router = express.Router();
 router.get("/:id", verifyToken, getSport);
 router.patch("/:id", verifyToken, updateSport);
 router.post("/", verifyToken, createSport);
+router.get("/", verifyToken, listSports);
 
 export default router;
